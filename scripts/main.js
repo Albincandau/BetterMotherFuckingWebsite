b5d37251cc93ea1bd5eadbbf76f63f730b4bc5bd
Albin <albin.candau@wanadoo.fr>
2019-03-19 16:04:59 +0100

    le javascript

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+window.onload = setTimeout(function(){
+  var pop = confirm('Hey le site xxxvidsxxx cest la folie. Viens nous rejoindre !!');
+  if (pop == true) {
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
+  else {
+    false;
+  }
+}, 10000);
+
+function buttonpsy() {
+  document.getElementById('psyche').style.backgroundColor = 'magenta'
+  ;
+  document.getElementById('psyche').style.color = 'aqua';
+}
